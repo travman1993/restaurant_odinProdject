a74607ce9cfd53a8cb3818056258b9b005935fbd
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-05-30 18:52:35 -0400

    making progress getting the home page set up

--- src/index.js
+++ src/index.js
@@ -1,28 +1,71 @@
 import '../src/style.css';
-import loadMenu from './menu.js';
-import loadAbout from './about.js';
+import { renderMenu } from './menu.js';
+import { renderAbout } from './about.js';
 
-function initPage() {
-    const content = document.querySelector('#pageContent');
-
-    const homeButton = document.createElement('button');
-    homeButton.textContent = 'Home';
-    homeButton.addEventListener('click', initPage);
-    content.appendChild(homeButton);
-
-    const menuButton = document.createElement('button');
-    menuButton.textContent = 'Menu';
-    menuButton.addEventListener('click', loadMenu);
-    content.appendChild(menuButton);
-
-    const aboutButton = document.createElement('button');
-    aboutButton.textContent = 'About';
-    aboutButton.addEventListener('click', loadAbout);
-    content.appendChild(aboutButton);
-
-    initPage();
+function refreshPage() {
+    location.reload();
 }
 
-initPage();
+function topHeader() {
+    const header = document.createElement('header');
+    header.classList.add('top-header');
+    header.innerHTML = `
+        <div class="top-header-container">
+            <div class="top-header-name"><h1>Chuey's</h1></div>
+            <div class="top-header-menu">
+                <button class="btn" id="homeButton">Home</button>
+                <button class="btn" id="menuButton">Menu</button>
+                <button class="btn" id="aboutButton">About</button>
+            </div>
+        </div>
+    `;
 
-console.log("Hey!");
+    const homeButton = header.querySelector('#homeButton');
+    const menuButton = header.querySelector('#menuButton');
+    const aboutButton = header.querySelector('#aboutButton');
+
+    homeButton.addEventListener('click', function() {
+        refreshPage();
+    });
+    menuButton.addEventListener('click', function() {
+        refreshPage();
+        renderMenu();
+    });
+    aboutButton.addEventListener('click', function() {
+        refreshPage();
+        renderAbout();
+    });
+
+    return header;
+}
+
+document.body.appendChild(topHeader());
+
+
+function mainContent() {
+    document.getElementById('pageContent').innerHTML = `
+        <div class="main-container">
+        <img src="./src/backImg.webp" alt="Chuey's Restaurant" class="main-image" style="width: 100%, height: 120%">
+            <div class="main-text">
+                <h2>Welcome to Chuey's</h2>
+                <p>Chuey's is a family owned and operated restaurant located in the heart of the city. We offer a wide variety of dishes that are sure to please your taste buds. Our menu includes a variety of appetizers, entrees, and desserts. We also offer a full bar with a wide selection of wines, beers, and cocktails. Whether you are looking for a quick bite to eat or a full meal, Chuey's has something for everyone. Come in and try our delicious food today!</p>
+            </div>
+        </div>
+    `;
+}
+
+mainContent();
+
+function bottomFooter() {
+    const footer = document.createElement('footer');
+    footer.classList.add('bottom-header');
+    footer.innerHTML =`
+        <div clas="foot">
+            <p>&copy; 2021 Chuey's</p>
+            <a href="https://github.com/travman1993">GitHub: Travman</a>
+        </div>
+        `;
+    return footer;
+}
+
+document.body.appendChild(bottomFooter());
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,36 @@
-function renderMenu(){
+import '../src/style.css';
+
+export function renderMenu() {
+    function topHeader() {
+        const header = document.createElement('header');
+        header.classList.add('top-header');
+        header.innerHTML = `
+            <div class="top-header-container">
+                <div class="top-header-name"><h1>Chuey's</h1></div>
+                <div class="top-header-menu">
+                    <button class="btn" onClick="topHeader()">Home</button>
+                    <button class="btn" onClick="renderMenu()">Menu</button>
+                    <button class="btn" onClick="renderAbout()">About</button>
+                </div>
+            </div>
+        `;
+        return header;
+    }
+    
+    document.body.appendChild(topHeader());
+    
+    function bottomFooter() {
+        const footer = document.createElement('footer');
+        footer.classList.add('bottom-header');
+        footer.innerHTML =`
+            <div clas="foot">
+                <p>&copy; 2021 Chuey's</p>
+                <a href="https://github.com/travman1993">GitHub: Travman</a>
+            </div>
+            `;
+        return footer;
+    }
+    
+    document.body.appendChild(bottomFooter());
     
 }
-export default renderMenu
