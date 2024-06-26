a6e3dda3d51509235826e39399f509c95073c3e3
Travis Rodriguez <travisgolembiewski@gmail.com>
2024-06-25 21:41:48 -0400

    new

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    homePage: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./src",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Chueys",
+      template: "./src/home.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.webp$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
